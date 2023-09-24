ad4c300fa51fd0c9e6a838f50dab14bb49bdbedd
Za Breado <sergiufulop45@gmail.com>
2023-09-24 13:30:52 +0300

    conversion operators 1

---
+++ Conversion operators/c_style_cast_operator.cpp
@@ -0,0 +1,16 @@
+/*
+    Conversion Operators
+    1. C conversion operaor:
+     ()  -> C Style Cast Operator
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c = 'A';
+    int cod = (int)c;
+
+    cout << cod;
+}
