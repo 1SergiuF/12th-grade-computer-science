e9b74027e4173e999467656ba9debfe4b0211ed1
Za Breado <sergiufulop45@gmail.com>
2023-10-08 21:20:11 +0300

    DictionaryExample C# project

---
+++ C#/DictionaryExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string str = Console.ReadLine();
+            int n = int.Parse(str);
+            Dictionary<int, char> keyValuePairs = new Dictionary<int, char>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                keyValuePairs.Add(i + 1, chars[rand.Next(chars.Length)]);
+            }
+
+            foreach (var kv in keyValuePairs)
+                Console.WriteLine($"{kv.Key}. {kv.Value}");
+
+            Console.ReadKey();
+        }
+    }
+}
