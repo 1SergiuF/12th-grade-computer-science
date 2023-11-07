2df1c986df66587fcb75e33734b94fed6045081f
Za Breado <sergiufulop45@gmail.com>
2023-11-07 20:17:58 +0200

    Tablouri unidimensionale 1

---
+++ C#/TablouriUnidimensionale1/TU1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablouriUnidimensionale1
+{
+    internal class TU1
+    {
+        static void Main(string[] args)
+        {
+            string[] a = null;  // declaram o referinta spre uin viitor tablou
+            a = new string[]
+            {
+                "Nicu",
+                "Ana",
+                "Sergiu",
+                "Eren"
+            };
+
+            // Parcurgeri
+            for (int i = 0; i < a.Length; i++)
+            {
+                //Console.Write(a[i] + " ");
+                //Console.Write($"{a[i]} ");
+                Console.Write("{0} ", a[i]);
+            }
+
+            Console.WriteLine();
+
+            foreach (string s in a)
+                Console.Write(s + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/TablouriUnidimensionale1/TU2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablouriUnidimensionale1
+{
+    internal class TU2
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };  // ok
+
+            int n = int.Parse(Console.ReadLine());
+            a = new int[n];   // alocare run-time
+            for (int i = 0; i < a.Length; i++)
+                a[i] = i + 1;
+
+            Console.WriteLine();
+
+            foreach (int x in a)
+                Console.Write(x + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
